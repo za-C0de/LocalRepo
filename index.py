cdc91e041fb9017a16796d6281c5cd05e8cd83de
Coursara-Test <limbubiplove708@gmail.com>
2025-07-23 09:04:23 +0545

    Python

---
+++ index.py
@@ -0,0 +1,9 @@
+# This is for python on git-hub !
+guys = "you"
+me = "and"
+games = "I"
+yogesh = 99;
+title = "maybe"
+this = "";
+when = "something"
+print(guys , "" , me , "" , games , "" , yogesh , "" , title + "" , when);
